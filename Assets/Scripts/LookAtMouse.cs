5cb952506e4b635e5fa037265b2a0840cf759c01
byFredi <142809594+byFredi@users.noreply.github.com>
2023-08-23 20:31:07 +0200

    new

---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    private Transform m_transform;
+
+    private void Start()
+    {
+        m_transform = this.transform;
+    }
+
+    private void LAMouse()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint
+            (Input.mousePosition) - m_transform.position;
+         float angle = Mathf.Atan2(direction.y, direction.x) *
+            Mathf.Rad2Deg;
+
+
+         Quaternion rotation = Quaternion.AngleAxis(angle - 90,
+            Vector3.forward);
+         m_transform.rotation = rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LAMouse();
+    }
+}

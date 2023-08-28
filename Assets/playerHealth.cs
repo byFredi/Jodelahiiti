351cc67dd022f2b3396aa2bd00cd98c055895f7d
byFredi <142809594+byFredi@users.noreply.github.com>
2023-08-28 10:55:26 +0200

    Healthbar start

---
+++ Assets/playerHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
